3491e3d9d4ab3141624ef96da8b1a95e4afb5355
임하정 <imhajeong@imhajeong-ui-MacBookPro.local>
2025-02-03 02:30:14 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/sideproject/coffeechat/CoffeeChatApplication.java
@@ -0,0 +1,13 @@
+package sideproject.coffeechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeChatApplication.class, args);
+	}
+
+}
